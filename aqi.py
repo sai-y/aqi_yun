28e2439923c4b4e9c1a7d949a4fa19bc4165a72b
sai-y <yamanoorsai@gmail.com>
2018-05-01 08:24:35 -0400

    Second check in

--- aqi.py
+++ aqi.py
@@ -1,4 +1,15 @@
 import requests
+import configparser
+import os
+
+KEY_FILE = "/root/key.ini"
+def get_key():
+	config = configparser.ConfigParser()
+	if os.path.isfile("/root/key.ini"):
+		config.read(KEY_FILE)
+		return config.get("TOKEN", "API_KEY")
 
 
 if __name__ == "__main__":
+	api_key = get_key()
+	print(api_key)
